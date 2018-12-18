ad4de489982c3a0702f7f19c6d5d1478b615171e
Swapnil Gadekar <swapnil.gadekar@medlife.com>
2018-12-19 01:08:34 +0530

    collabrator init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-layout align-center justify-center>
+        <v-flex xs12>
+         <v-card color="#549EFF">
+           <v-card-text >
+             <div class="text-md-center title-header" text-lg-center>
+               Collabrative Document editor
+             </div> 
+           </v-card-text>
+         </v-card>
+        </v-flex>
+      </v-layout>
+      <v-layout align-baseline class="top-margin">
+        <v-flex offset-xs2 xs6>
+          <v-text-field label="UserName" v-model="username" placeholder="Please enter a username to start editing"></v-text-field>
+        </v-flex>
+        <v-flex offset-xs1 xs3>
+          <v-btn @click="userValidity" color="">Enter</v-btn>
+        </v-flex>
+      </v-layout>
+      <editor  v-if="validUser" :username="this.username" />
+      <v-snackbar
+      v-model="snackbar"
+      :timeout="timeout"
+      :bottom="y === 'bottom'"
+      :left="x === 'left'"
+      :right="x === 'right'"
+      :top="y === 'top'"
+      >
+      {{text}}
+      </v-snackbar>
+    </v-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Editor from "./components/Editor.vue";
+import io from "socket.io-client";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld,
+    Editor
   },
+  data() {
+    return {
+      username: "",
+      validUser: false,
+      value: "",
+      snackbar:false,
+      y: 'top',
+      x: 'right',
+      timeout:6000,
+      text:'',
+      sockets: io.connect("http://localhost:5000")
+    };
+  },
+  mounted(){
+    this.sockets.on("released", () => {
+      this.text = 'Now you can start editing';
+      this.snackbar = true;
+    });
+  },
+  methods: {
+    userValidity() {
+      if (this.username !== "") {
+        axios
+          .post("http://localhost:5000/adduser", { username: this.username })
+          .then(res => {
+            res.status == 200 ? (this.validUser = true) : "";
+          })
+          .catch(err => {
+            this.text = 'User name already in use';
+            this.snackbar = true;
+            console.log(err);
+          });
+      }
+    }
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.title-header{
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: bolder;
+}
+.top-margin{
+  margin-top: 5%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
+import '../node_modules/quill/dist/quill.bubble.css';
+import '../node_modules/quill/dist/quill.snow.css';
+import '../node_modules/quill/dist/quill.core.css';
 
 Vue.config.productionTip = false;
 
